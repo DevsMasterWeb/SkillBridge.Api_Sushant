6b4793688d469163be3b576842fa4d661a2285ea
Sushant Subedi <subedisushant781@gmail.com>
2026-05-14 07:55:58 +0545

    Adding entity classes and migration those classes to database

---
+++ SkillBridge.Api/Entities/UserProfile.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using SkillBridge.Api.Entities;
+
+
+public class UserProfile
+{
+    [Required, Key]
+    public int Id {get; set;}
+    [Required]
+    public User User {get; set;}
+    public int UserId {get; set;}
+    [Required]
+    public string FullName {get; set;}
+    [Required]
+    public string SkillSet {get; set;}
+    [Required]
+    public string Experience {get; set;}
+    [Required]
+    public string Education {get; set;}
+    [Required]
+    public string ResumePath {get; set;}
+    public string LinkedInProfile {get; set;}
+    public string GithubProfile {get; set;}
+    [Required]
+    public string Bio {get; set;}
+    [Required]
+    public string Location {get; set;}
+    [Required]
+    public string ContactNumber {get; set;}
+        
+    }
--- SkillBridge.Api/SkillBridgeDbContext.cs
+++ SkillBridge.Api/SkillBridgeDbContext.cs
@@ -10,4 +10,9 @@ public class SkillBridgeDbContext : DbContext
 
 
     public DbSet<User> Users {get; set;}
+    public DbSet<Job> Jobs {get; set;}
+    public DbSet<JobApplication> JobApplications {get; set;}
+    public DbSet<UserProfile> UserProfile {get; set;}
+
 }
+
